ca47917bb8fc35f31f96c297ac606cca94c09859
Steopa2001 <moraru495@gmail.com>
2025-11-28 13:14:53 +0100

    [feat] integrate AI response generation into chat

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ const messages = [
   },
 ];
 
+//Preparo l'indirizzo da chiamare per le API
+const endpoint = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyB484pGiJvh-Hya170oko3-foH3lh6hmf0`;
+const systemPrompt =
+  "Sei Stefan Moraru e hai 24 anni, il 9 gennaio 2026 ne avrai 25. Sei un amico che risponde in modo amichevole e informale. Sei uno sviluppatore front-end alle prime armi che ha imparato JavaScript come primo linguaggio. Sei determinato a lavorare in questo settore e ti piace lo sport: hai giocato a calcio per 12 anni e ora ogni tanto giochi ancora con gli amici. Rispondi in modo amichevole e informale, in italiano, come farebbe un amico in chat. Mantieni le risposte brevi e spontanee.";
+
 //Operazioni di Avvio Pagina
 showMessages();
 
@@ -25,8 +30,8 @@ button.addEventListener("click", sendMessage);
 
 //Alla pressione del tasto invio
 input.addEventListener("keydown", function (event) {
-    //Controllo se il tasto cliccato è 'Invio'
-    if(event.key === 'Enter') sendMessage()
+  //Controllo se il tasto cliccato è 'Invio'
+  if (event.key === "Enter") sendMessage();
 });
 
 //FUNZIONI UTILI
@@ -46,6 +51,12 @@ function showMessages() {
       </div>
     `;
   }
+    //Riporto il cursone sulla casella
+  input.focus();
+
+  //Scorro in automatico alla fine del box
+  chatBox.scrollTop = chatBox.scrollHeight;
+
 }
 
 //Funzione per aggiungere un messaggio
@@ -75,9 +86,54 @@ function sendMessage() {
 
   //Svuoto la casella di testo
   input.value = "";
-  //Riporto il cursone sulla casella
-  input.focus();
 
-  //Scorro in automatico alla fine del box
-  chatBox.scrollTop = chatBox.scrollHeight;
+  //Chiedo a Gemini di generare una risposta
+  getAnswerFromGemini()
+}
+
+// IMPLEMENTAZIONE AI
+
+//funzione per formattare la chat in un formato gradito a Gemini
+function formatChatForGemini() {
+  //Preparo un array per la "Nuova Chat"
+  const formattedChat = [];
+
+  for (const message of messages) {
+    //Creo e aggiungo un nuovo oggetto alla mia chat formattata
+    formattedChat.
+      push({
+        parts: [{ text: message.text }],
+        role: message.type === "sent" ? "user" : "model",
+      });
+  }
+
+  //Aggiungo il systemPrompt all'inizio dell'array
+  formattedChat.unshift({
+    role: "user",
+    parts: [{ text: systemPrompt }],
+  });
+
+  return formattedChat;
+}
+
+//Funzione per chiedere a Gemini di generare una risposta
+async function getAnswerFromGemini() {
+  //Prepariamo la chat da inviare
+  const chatForGemini = formatChatForGemini();
+
+  //Effettuiamo la chiamata API di Gemini
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ contents: chatForGemini }),
+  });
+
+  //Riconverto la risposta dal json 
+  const data = await response.json();
+
+  //Recupero il testo effettivo dalla risposta
+  const answer = data.candidates[0].content.parts[0].text
+
+  //Aggiungo il messaggio in pagina 
+  addMessage('received', answer)
 }
